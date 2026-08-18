e4e06bee6005f6d6d7a9d0123ba5daa37830d882
Sujal Malviya <sujalmalviya60@gmail.com>
2026-08-18 05:49:39 +0530

    Add Muliple Print Example

---
+++ 01-Basics/College.java
@@ -0,0 +1,8 @@
+public class College {
+    public static  void main(String[] args){
+        System.out.println("College Name Arya ");
+        System.out.println("Branch CSE");
+        System.out.println("5th semester");
+    }
+
+}
