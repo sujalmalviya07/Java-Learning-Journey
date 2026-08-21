15c1e1d391360775c972e85befcccc28838fa53b
Sujal Malviya <sujalmalviya60@gmail.com>
2026-08-21 23:34:49 +0530

    Add new java folder Operators If-else  and code

---
+++ Operators If-else & Number system/TypeConverion/Explicit.java
@@ -0,0 +1,10 @@
+public class Explicit {
+    public  static void main(String[] args){
+        float marks =  (float) 123.33d;
+        System.out.println(marks);
+
+        double percentage = 70.33d;
+        int result = (int) percentage;
+        System.out.println(result);
+    }
+}
---
+++ Operators If-else & Number system/TypeConverion/Implicit.java
@@ -0,0 +1,10 @@
+public class Implicit {
+    public static void main(String[] args){
+        int number = 15;
+        double total = number;
+        System.out.println("Total : "+total );
+        char value = 'A';
+        int characterValue = value;
+        System.out.println("A character value is : "+characterValue);
+    }
+}
