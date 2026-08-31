5b98de7e0cfd40a5d761892b4709cbc503a4d3b4
Sujal Malviya <sujalmalviya60@gmail.com>
2026-08-31 22:55:55 +0530

    Practice Question Add

--- 05 While Loop & Method/Practice Questions/LeatCommonDivision.java
+++ 05 While Loop & Method/Practice Questions/LeatCommonDivision.java
@@ -4,21 +4,28 @@ public class LeatCommonDivision {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Number ");
-        int num1 = sc.nextInt();
+        int num1 =  sc.nextInt();
         System.out.println("Enter Your Number ");
-        int num2 = sc.nextInt();
-        int i=1;
-        int sum=0;
-        int sum1 =0;
+        int num2 =  sc.nextInt();
 
-        while (i<=10){
-          sum=i*num1;
-          sum1=i*num2;
-          if(i%sum==0 && i%sum1==0){
+        int i =1;
+        while (true){
+            int multiple1=num1*i;
 
-          }
+            int j=1;
+            while (true){
+                int multiple2 = num2*j;
+                if(multiple1==multiple2){
+                    System.out.println("LCM " + multiple1);
+                    return;
+                }
+                if(multiple2>multiple1){
+                    break;
+                }
+                j++;
+            }
             i++;
         }
-        System.out.println(i);
+
     }
 }
