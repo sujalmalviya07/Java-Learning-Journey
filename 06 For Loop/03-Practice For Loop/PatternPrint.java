87eec160d6d437dfbc4dce00cdd5dfe94c4efeeb
Sujal Malviya <sujalmalviya60@gmail.com>
2026-09-22 22:23:53 +0530

    Add new practice code

--- 06 For Loop/03-Practice For Loop/PatternPrint.java
+++ 06 For Loop/03-Practice For Loop/PatternPrint.java
@@ -1,8 +1,8 @@
 public class PatternPrint {
     public static void main(String[] args){
         for(int i=1; i<=4 ; i++){
-            for (int j=0 ; j<i ; j++){
-                System.out.print("*"+" ");
+            for (int j=1; j<=i ; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
---
+++ 07 Methods & Return Types/Basic Level-Method/IntegerMethod.java
@@ -0,0 +1,12 @@
+public class IntegerMethod {
+
+    static int square(){
+        return 5;
+    }
+    public static void main(String[] args){
+        int x= square();
+       int y= 5*x;
+        System.out.println(y);
+    }
+
+}
